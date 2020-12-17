936362bff670eb17bfc86fbfe4813171b75e3a5d
Rudy-Menjivar <rudy.menjivar@gmail.com>
2020-12-17 15:43:13 -0800

    Set public folder as static to serve its static assets

--- server.js
+++ server.js
@@ -2,3 +2,5 @@
 var express = require("express");
 // Tells node to create an express server
 var server = express();
+// Set public folder as static to serve its static assets
+server.use(express.static('public'));
