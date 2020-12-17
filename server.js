6cf360bca797f232142b7eeac44c21078ec97746
Rudy-Menjivar <rudy.menjivar@gmail.com>
2020-12-17 15:46:35 -0800

    Sets whatever environment var port, or 8080 if nothing's available

--- server.js
+++ server.js
@@ -4,3 +4,5 @@ var express = require("express");
 var server = express();
 // Set public folder as static to serve its static assets
 server.use(express.static('public'));
+// Sets whatever environment var port, or 8080 if nothing's available
+const PORT = process.env.PORT || 8080;
